7afe6fa437f0d0da9982b3a6cfbfec8750767bb3
FuSoraS <developer.FuSoraS@gmail.com>
2026-04-04 18:09:34 -0300

    feat: update responsive
    Header links for slug-based navigation, add responsive menu, and enhance integrante pages with new layout and social links

--- src/content.config.ts
+++ src/content.config.ts
@@ -2,32 +2,19 @@ import { defineCollection } from 'astro:content';
 import { glob } from 'astro/loaders';
 import { z } from 'astro/zod';
 
-const blog = defineCollection({
-	// Load Markdown and MDX files in the `src/content/blog/` directory.
-	loader: glob({ base: './src/content/blog', pattern: '**/*.{md,mdx}' }),
-	// Type-check frontmatter using a schema
-	schema: ({ image }) =>
-		z.object({
-			title: z.string(),
-			description: z.string(),
-			// Transform string to Date object
-			pubDate: z.coerce.date(),
-			updatedDate: z.coerce.date().optional(),
-			heroImage: z.optional(image()),
-		}),
-});
-
 const integrantes = defineCollection({
 	// Load Markdown and MDX files in the `src/content/integrantes/` directory.
 	loader: glob({ base: './src/content/integrantes', pattern: '**/*.{md,mdx}' }),
 	// Type-check frontmatter using a schema
-	schema: ({ image }) =>
-		z.object({
-			title: z.string().optional(),
-			name: z.string().optional(),
+	schema: z.object({
+			name: z.string(),
 			description: z.string(),
 			cover: z.string().optional(),
+			youtube: z.string().optional(),
+			instagram: z.string().optional(),
+			x: z.string().optional(),
+			tiktok: z.string().optional(),
 		}),
 });
 
-export const collections = { blog, integrantes };
+export const collections = { integrantes };
