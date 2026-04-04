0ca3a429b012e8e6fb61756816f5e839e5f695a5
FuSoraS <developer.FuSoraS@gmail.com>
2026-04-04 11:17:14 -0300

    refactor: replace favicon, add new images, and enhance layout for integrantes

--- src/consts.ts
+++ src/consts.ts
@@ -1,5 +1,5 @@
 // Place any global data in this file.
 // You can import this data from anywhere in your site by using the `import` keyword.
 
-export const SITE_TITLE = 'Astro Blog';
-export const SITE_DESCRIPTION = 'Welcome to my website!';
+export const SITE_TITLE = 'Tonotos Crew';
+export const SITE_DESCRIPTION = 'Un grupo creado con la finalidad de proporcionar apoyo a creadores de contenido variado en su crecimiento en las redes.';
