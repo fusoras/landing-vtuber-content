d364e0d683fa5445e14b1de8098091c0529cc969
FuSoraS <developer.FuSoraS@gmail.com>
2026-05-11 12:36:38 -0400

    refactor: remove svelte

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,13 +1,11 @@
 // @ts-check
 
-import mdx from '@astrojs/mdx';
-import sitemap from '@astrojs/sitemap';
-import { defineConfig } from 'astro/config';
-
-import svelte from '@astrojs/svelte';
+import mdx from "@astrojs/mdx";
+import sitemap from "@astrojs/sitemap";
+import { defineConfig } from "astro/config";
 
 // https://astro.build/config
 export default defineConfig({
-    site: 'https://example.com',
-    integrations: [mdx(), sitemap(), svelte()],
+  site: "https://example.com",
+  integrations: [mdx(), sitemap()],
 });
